dc0def5a823f53859869b064017f79dd1fbd997c
Michael McEniry (UAH) <mcenirm@uah.edu>
2015-04-01 19:55:35 -0500

    use docker for more authentic pip package results

--- refresh-requirements.bash
+++ refresh-requirements.bash
@@ -2,11 +2,14 @@
 
 top=$(cd $(dirname "$0") && /bin/pwd)
 cache=${top}/cache
-reqs=${top}/geoevents/geoevents/requirements.txt
+geoevents=${top}/geoevents
+reqs=${geoevents}/geoevents/requirements.txt
 dockerfile=${top}/Dockerfile
 
 [[ -d "$cache" ]] || mkdir -p "$cache" || exit $?
 
-for req in $(< "$reqs") ; do
-  pip install --download "$cache" "$req" || exit $?
-done
+docker run --rm -it \
+    -v "$cache":/tmp/cache \
+    -v "$geoevents":/tmp/geoevents \
+    $(sed -n -e 's/^FROM //p' "$dockerfile") \
+    pip install --download /tmp/cache -r /tmp/geoevents/geoevents/requirements.txt
